ec0bf12f52000569bd4d0bb97a8e63a2ce5f453f
Sophie Stupalo <55926599+sstupalo@users.noreply.github.com>
2022-08-19 23:56:45 -0400

    Add experience page content

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -3,11 +3,40 @@ import { PageHeader } from '../global/Global';
 import {
   ExperiencePageContainer
 } from './ExperienceStyles';
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
+import TimelineDot from '@mui/lab/TimelineDot';
 
 function Experience({data}) {
   return (
     <ExperiencePageContainer>
       <PageHeader title="Experience"/>
+      <Timeline>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Eat</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Code</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+        </TimelineSeparator>
+        <TimelineContent>Sleep</TimelineContent>
+      </TimelineItem>
+    </Timeline>
     </ExperiencePageContainer> 
   )
 }
