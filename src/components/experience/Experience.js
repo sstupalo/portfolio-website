c1b046123b8b817f78a66beb24b0b5a5969ab67f
Sophie Stupalo <55926599+sstupalo@users.noreply.github.com>
2022-08-20 22:12:56 -0400

    Create experience page timeline

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,42 +1,46 @@
 import React from 'react';
 import { PageHeader } from '../global/Global';
 import {
-  ExperiencePageContainer
+  ExperiencePageContainer,
+  CompanyText,
+  PositionText,
+  DateText,
+  ContentText,
+  CompanyImage
 } from './ExperienceStyles';
-import Timeline from '@mui/lab/Timeline';
-import TimelineItem from '@mui/lab/TimelineItem';
-import TimelineSeparator from '@mui/lab/TimelineSeparator';
-import TimelineConnector from '@mui/lab/TimelineConnector';
-import TimelineContent from '@mui/lab/TimelineContent';
-import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
-import TimelineDot from '@mui/lab/TimelineDot';
+import { 
+  Timeline,
+  TimelineItem,
+  TimelineSeparator,
+  TimelineConnector,
+  TimelineContent,
+  TimelineOppositeContent
+} from '@mui/lab';
 
 function Experience({data}) {
   return (
     <ExperiencePageContainer>
       <PageHeader title="Experience"/>
-      <Timeline>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>Eat</TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>Code</TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot />
-        </TimelineSeparator>
-        <TimelineContent>Sleep</TimelineContent>
-      </TimelineItem>
-    </Timeline>
+      <Timeline> 
+        {data.experienceList.map((experienceList) => {
+          return (
+            <TimelineItem>
+              <TimelineOppositeContent>
+                <CompanyText>{experienceList.company}</CompanyText>
+                <PositionText>{experienceList.position}</PositionText>
+                <DateText>{experienceList.dates}</DateText>
+              </TimelineOppositeContent>
+              <TimelineSeparator>
+                <CompanyImage src={experienceList.image}/>
+                <TimelineConnector />
+              </TimelineSeparator>
+              <TimelineContent>
+                <ContentText>{experienceList.content}</ContentText>
+              </TimelineContent>           
+            </TimelineItem>
+          );
+        })}
+      </Timeline>
     </ExperiencePageContainer> 
   )
 }
--- src/components/experience/experienceStyles.js
+++ src/components/experience/experienceStyles.js
@@ -7,5 +7,66 @@ export const ExperiencePageContainer = styled.div`
   background-color: #FFFFFFFF;
   display: flex;
   flex-direction: column;
-  height: calc(100vh - 80px);
+  height: 100%;
+`;
+
+export const CompanyText = styled.h1`
+  color: #2B2B2BFF;
+  font-family: 'captura';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20pt;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  margin-top: 0%;
+  margin-bottom: 0%;
+  white-space: nowrap; 
+`
+
+export const PositionText = styled.h2`
+  color: #747474FF;
+  font-family: 'captura';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18pt;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  margin-top: 0%;
+  margin-bottom: 0%;
+  white-space: nowrap; 
+`
+
+export const DateText = styled.h3`
+  color: #FFC9C9FF;
+  font-family: 'captura';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16pt;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  margin-top: 0%;
+  margin-bottom: 0%;
+  white-space: nowrap; 
+`
+
+export const ContentText = styled.p`
+  color: #2B2B2BFF;
+  font-family: 'carniola';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16pt;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  margin-top: 0%;
+  margin-bottom: 0%;
+`
+
+export const CompanyImage = styled.img`
+  width: 100px;
+  height: auto;
+  filter: drop-shadow(2px 2px 4px #000000AA);
 `;
