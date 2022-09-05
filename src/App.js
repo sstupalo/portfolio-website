7c9fbb612974af3fcd32feb942b2b3ce24814228
Sophie Stupalo <55926599+sstupalo@users.noreply.github.com>
2022-09-05 13:20:43 -0400

    Replace BrowserRouter with HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Home from './components/home/Home';
 import Experience from './components/experience/Experience';
