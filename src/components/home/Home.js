c01f9a40f801adde4b31cf09c794dddcc9461349
Sophie Stupalo <55926599+sstupalo@users.noreply.github.com>
2024-12-16 12:41:41 -0500

    Removed old components

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import { PageHeader, SubHeading } from '../global/Global';
+import { PageHeader} from '../global/Global';
 import {
   HomePageContainer,
   ContentContainer,
   LeftContainer,
   RightContainer,
   BottomContainer,
-  TopContentContainer,
-  DetailsContainer,
-  StyledFontAwesomeIcon,
-  GradCapIcon,
-  DownloadIcon,
-  DownloadButton,
-  LinkContainer,
-  ButtonText,
-  DownloadButtonText,
   AboutMeParagraph,
   ProfilePicture
 } from './HomeStyles';
-import { faGraduationCap, faPhone, faEnvelope, faDownload } from '@fortawesome/free-solid-svg-icons'
 
 
 function Home({data}) {
@@ -27,29 +17,6 @@ function Home({data}) {
       <PageHeader title='Sophie Stupalo'/>
       <ContentContainer> 
         <LeftContainer>
-          {/* <TopContentContainer>
-            <DetailsContainer> 
-              <SubHeading title='Contact Details'/>
-              <LinkContainer href={data.phoneLink}>
-                <StyledFontAwesomeIcon icon={faPhone}/><ButtonText>{data.phoneText}</ButtonText>
-              </LinkContainer>
-              <LinkContainer href={data.emailLink}>
-                <StyledFontAwesomeIcon icon={faEnvelope}/><ButtonText>{data.emailText}</ButtonText>
-              </LinkContainer>
-            </DetailsContainer>
-            <DetailsContainer> 
-              <SubHeading title='Education'/>
-              <LinkContainer href={data.educationLink}  target='_blank' rel='noopener noreferrer'>
-                <GradCapIcon icon={faGraduationCap}/><ButtonText>University of Waterloo<br />3B Biomedical Engineering</ButtonText>
-              </LinkContainer>
-            </DetailsContainer>
-          </TopContentContainer> */}
-          {/* <LinkContainer href={data.resumeLink} download>
-            <DownloadButton> 
-              <DownloadIcon icon={faDownload}/><DownloadButtonText>Download Resume</DownloadButtonText>
-            </DownloadButton>
-          </LinkContainer> */}
-          {/* <SubHeading title='About:'/> */}
           <AboutMeParagraph>{data.aboutMeParagraph}</AboutMeParagraph>
         </LeftContainer>
         <RightContainer>
